3624724c2fdf424ca1e27338cddf3373b5952b67
Evan McDaniel <eamigo13@gmail.com>
2017-12-13 15:35:25 -0700

    This is my first commit.  I've come up with a proof of concept using the About View and the Home Controller.  I'm committing these changes so I can keep the proof of concept intact and start to work on the real program.

--- PersonalFinanceApplication/Controllers/HomeController.cs
+++ PersonalFinanceApplication/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.VisualBasic.FileIO;
+using PersonalFinanceApplication.Models;
 
 namespace PersonalFinanceApplication.Controllers
 {
@@ -17,7 +19,34 @@ namespace PersonalFinanceApplication.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            var parser = new TextFieldParser(@"c:\Users\eamig\source\repos\PersonalFinanceApplication\PersonalFinanceApplication\Content\csv\bk_download.csv");
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters(new string[] { "," });
+
+            ViewBag.table = new List<string[]>();
+
+            var columns = new List<Column>();
+
+            string[] firstrow = parser.ReadFields();
+
+            for(int i=0; i<firstrow.Length; i++)
+            {
+                Column column = new Column();
+                column.ColumnNumber = i;
+                column.ExampleValue = firstrow[i];
+                columns.Add(column);
+            }
+
+            while (!parser.EndOfData)
+            {
+                string[] row = parser.ReadFields();
+                /* do something */
+                ViewBag.table.Add(row);
+            }
+
+            
+
+            return View(columns);
         }
 
         public ActionResult Contact()
---
+++ PersonalFinanceApplication/Models/Column.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PersonalFinanceApplication.Models
+{
+    public class Column
+    {
+        public int ColumnNumber { get; set; }
+        public string ExampleValue { get; set; }
+    }
+}
