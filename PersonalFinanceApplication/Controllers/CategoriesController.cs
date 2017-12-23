baa429b31096662502a41e4adad72ff6c22e39db
Evan McDaniel <eamigo13@gmail.com>
2017-12-23 16:02:59 -0700

    Changes made that allows you to add a new category while confirming transactions

---
+++ PersonalFinanceApplication/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using PersonalFinanceApplication.DAL;
+using PersonalFinanceApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalFinanceApplication.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private FinanceContext db = new FinanceContext();
+
+        /* POST -- /Vendors/Add
+         * This method adds a new Vendor to the db.
+         */
+        [HttpPost]
+        public JsonResult Add(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return Json(category);
+        }
+    }
+}
