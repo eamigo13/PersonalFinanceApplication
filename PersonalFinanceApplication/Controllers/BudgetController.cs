225c5bcecccce297017c5d964dbd4f2ccb5c57dd
Evan McDaniel <eamigo13@gmail.com>
2017-12-26 19:48:44 -0700

    Added pie charts

--- PersonalFinanceApplication/Controllers/BudgetController.cs
+++ PersonalFinanceApplication/Controllers/BudgetController.cs
@@ -22,7 +22,7 @@ namespace PersonalFinanceApplication.Controllers
         }
 
         //Get: Budgets/View
-        public ActionResult View(int? id)
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
@@ -78,7 +78,65 @@ namespace PersonalFinanceApplication.Controllers
 
             return View(budgetcategories);
         }
-        
+
+        //Get: Budgets/View
+        public ActionResult View(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Budget budget = db.Budgets.Find(id);
+            if (budget == null)
+            {
+                return HttpNotFound();
+            }
+
+            //Add the budget to the viewbag
+            ViewBag.budget = budget;
+
+            //Return all BudgetCategory rows associated with the budget
+            var query = @"  select 
+	                            bc.BudgetID,
+	                            bc.CategoryID,
+	                            bc.Amount,
+	                            isnull(( select sum(t.Amount) from [Transaction] t
+	                              where t.CategoryID = bc.CategoryID 
+	                              and t.Date 
+		                            between (select b.BeginDate from Budget b where b.BudgetID = bc.BudgetID) 
+		                            and (select b.EndDate from Budget b where b.BudgetID = bc.BudgetID) ), 0 ) as UsedAmount,
+	                            (bc.Amount - isnull(( select sum(t.Amount) from [Transaction] t
+	                              where t.CategoryID = bc.CategoryID 
+	                              and t.Date 
+		                            between (select b.BeginDate from Budget b where b.BudgetID = bc.BudgetID) 
+		                            and (select b.EndDate from Budget b where b.BudgetID = bc.BudgetID) ), 0 ) ) as RemainingAmount
+	
+                            from BudgetCategory bc
+                            where bc.BudgetID =" + id;
+
+            var budgetcategories = db.Database.SqlQuery<BudgetCategory>(query).ToList();
+
+            foreach (var bc in budgetcategories)
+            {
+                db.Entry(bc).State = EntityState.Modified;
+            }
+
+            db.SaveChanges();
+
+            //var budgetcategories = db.BudgetCategories.Where(b => b.BudgetID == id).ToList();
+
+
+
+            //Add categories to ViewBag
+            ViewBag.Categories = db.Categories.ToList();
+
+
+
+            //Add a select list of the categories
+
+            return View(budgetcategories);
+        }
+
         [HttpPost]
         public JsonResult AddCategory(BudgetCategory budgetcategory)
         {
@@ -143,36 +201,36 @@ namespace PersonalFinanceApplication.Controllers
             return View(budget);
         }
 
-        // GET: Budgets/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Budget budget = db.Budgets.Find(id);
-            if (budget == null)
-            {
-                return HttpNotFound();
-            }
-            return View(budget);
-        }
+        //// GET: Budgets/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Budget budget = db.Budgets.Find(id);
+        //    if (budget == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(budget);
+        //}
 
-        // POST: Budgets/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BudgetID,BudgetName,Description,BeginDate,EndDate")] Budget budget)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(budget).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(budget);
-        }
+        //// POST: Budgets/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "BudgetID,BudgetName,Description,BeginDate,EndDate")] Budget budget)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(budget).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(budget);
+        //}
 
         // GET: Budgets/Delete/5
         public ActionResult Delete(int? id)
